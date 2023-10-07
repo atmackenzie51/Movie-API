f5aa0d8eb78a727bd7009210924bb4a85f9cdda5
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2023-10-07 13:57:57 -0400

    commented out localhost code

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const {check, validationResults} = require("express-validator");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-//mongoose.connect('mongodb://127.0.0.1:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true});
+/*mongoose.connect('mongodb://127.0.0.1:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true});*/
 mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //setting up the logger
