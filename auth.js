31051df108b6f5cf6b2caac263e3a2ffad9689a3
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-06-30 14:23:15 -0700

    jsdoc

--- auth.js
+++ auth.js
@@ -3,18 +3,27 @@ const jwtSecret = 'your_jwt_secret'; // has to be the same key used in the JWTSt
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 
-require('./passport'); //the local passport file
+require('./passport'); // the local passport file
 
-
+/**
+ * Generates a JWT token for a user
+ * @function generateJWTToken
+ * @param {object} user - The user object
+ * @returns {string} - The JWT token
+ */
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
-        subject: user.Username, //username getting encoded with JWT
+        subject: user.Username, // username getting encoded with JWT
         expiresIn: '7d', // specifies token expires in 7 days
         algorithm: 'HS256' // this is the algorithm used to "sign" or encode values of the JWT
     });
 }
 
-/* POST Login. */
+/**
+ * POST login route
+ * @function
+ * @param {object} router - The Express router object
+ */
 module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', { session: false }, (error, user, info) => {
